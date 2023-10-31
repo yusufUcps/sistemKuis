af276e70543d50d09ef3509e795b793feb3604b2
yusuffy <34950818+yusuffy@users.noreply.github.com>
2023-11-01 06:16:15 +0700

    refacror: update helper jwt

--- helper/jwt.go
+++ helper/jwt.go
@@ -1,30 +1,44 @@
 package helper
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/sirupsen/logrus"
 )
 
-func GenerateJWT(signKey string, userID uint) string {
-	var accessToken = generateToken(signKey, userID)
+type JWTInterface interface {
+	GenerateJWT(userID uint) string
+	GenerateToken(id uint) string
+	ExtractToken(token *jwt.Token) uint
+}
+
+type JWT struct {
+	signKey string
+}
+
+func New(signKey string) JWTInterface {
+	return &JWT{
+		signKey: signKey,
+	}
+}
+
+func (j *JWT) GenerateJWT(userID uint) string {
+	var accessToken = j.GenerateToken(userID)
 	if accessToken == "" {
 		return ""
 	}
 	return accessToken
 }
 
-
-func generateToken(signKey string, id uint) string {
+func (j *JWT) GenerateToken(id uint) string {
 	var claims = jwt.MapClaims{}
 	claims["id"] = id
 	claims["iat"] = time.Now().Unix()
-	claims["exp"] = time.Now().Add(time.Minute * 10).Unix()
+	claims["exp"] = time.Now().Add(time.Hour* 1).Unix()
 
 	var sign = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	validToken, err := sign.SignedString([]byte(signKey))
+	validToken, err := sign.SignedString([]byte(j.signKey))
 
 	if err != nil {
 		return ""
@@ -33,19 +47,24 @@ func generateToken(signKey string, id uint) string {
 	return validToken
 }
 
+func (j *JWT) ExtractToken(token *jwt.Token) uint {
+    if token != nil {
+        claims, ok := token.Claims.(jwt.MapClaims)
+        if !ok {
+            logrus.Error("Invalid token claims")
+            return 0
+        }
 
-func ExtractToken(token *jwt.Token) uint {
-	if token.Valid {
-		var claims = token.Claims
-		expTime, _ := claims.GetExpirationTime()
-		fmt.Println(expTime.Time.Compare(time.Now()))
-		if expTime.Time.Compare(time.Now()) > 0 {
-			var mapClaim = claims.(jwt.MapClaims)
-			return uint(mapClaim["id"].(float64))
-		}
+        id, exists := claims["id"].(float64)
+        if !exists {
+            logrus.Error("ID not found in token claims")
+            return 0
+        }
 
-		logrus.Error("Token expired")
-	}
-	return 0
+        return uint(id)
+    }
+
+    logrus.Error("Invalid token")
+    return 0
 }
 
