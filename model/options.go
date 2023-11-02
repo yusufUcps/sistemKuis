e1a64678ae121371d3edef9ad7d79bc743acb155
yusuffy <34950818+yusuffy@users.noreply.github.com>
2023-11-02 16:30:22 +0700

    refactor: model questions and options

--- model/options.go
+++ model/options.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -8,5 +10,53 @@ type Options struct {
 	gorm.Model
 	Value       string `json:"value" gorm:"type:text"`
 	Question_id uint   `json:"question_id"`
-	Is_right bool `json:"is_right"`
+	Is_right 	bool `json:"is_right"`
 }
+
+type OptionsRes struct {
+	Id 				uint `json:"id"`
+	Created_at 		time.Time`json:"created_at" `
+    Updatad_at 		time.Time`json:"updated_at" `
+	Value       	string `json:"value"`
+	Question_id 	uint   `json:"question_id"`
+	Is_right 		bool `json:"is_right"`
+}
+
+type OptionsResForQuestion struct {
+	Id 				uint `json:"id"`
+	Value       	string `json:"value"`
+	Is_right 		bool `json:"is_right"`
+}
+
+
+func ConvertOptionsRes(option *Options) *OptionsRes {
+    optionRes := OptionsRes{
+        Id:         option.ID,
+        Created_at: option.CreatedAt,
+        Updatad_at: option.UpdatedAt,
+		Value:		option.Value,
+		Is_right: 	option.Is_right,
+    }
+    return &optionRes
+}
+
+func ConvertOptionsResForQuestion(option *Options) *OptionsResForQuestion {
+    optionRes := OptionsResForQuestion{
+        Id:         option.ID,
+		Value:		option.Value,
+		Is_right: 	option.Is_right,
+    }
+    return &optionRes
+}
+
+func ConvertAllOptions (options []Options) []OptionsResForQuestion{
+	
+	var optionRes []OptionsResForQuestion
+	
+	for i := range options {
+		optionRes = append(optionRes, *ConvertOptionsResForQuestion(&options[i]))
+    }
+
+	return optionRes
+}
+
