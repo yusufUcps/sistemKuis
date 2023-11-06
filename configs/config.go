2ded50c581c22ffa6c91b805c41d47ca28d9143c
yusuffy <34950818+yusuffy@users.noreply.github.com>
2023-11-06 15:56:54 +0700

    config

--- configs/config.go
+++ configs/config.go
@@ -84,6 +84,8 @@ func loadConfig() *ProgramConfig {
 
 	if val, found := os.LookupEnv("PRIVATE_KEY"); found {
 		cleanedVal := strings.ReplaceAll(val, "*", " ")
+		cleanedVal = strings.ReplaceAll(cleanedVal, "(", "")
+		cleanedVal = strings.ReplaceAll(cleanedVal, ")", "")
 		res.PrivateKey = cleanedVal
 	}
 
